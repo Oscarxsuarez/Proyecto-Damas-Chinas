2255d58ccb076887bdcf4bc58e00eb68d657b8af
Oscarxsuarez <oscar252423@gmail.com>
2025-11-12 18:07:41 -0500

    Refactor tablero.cpp for improved readability

--- tablero.cpp
+++ tablero.cpp
@@ -1,9 +1,14 @@
 #include "pieza.h"
 #include "tablero.h"
 #include <iostream>
+#include <string>
 
-using namespace std;
-
+#define RESET   "\033[0m"
+#define NEGRO   "\033[30m"
+#define BLANCO  "\033[97m"
+#define FONDO_OSCURO "\033[48;5;236m"   // gris oscuro
+#define FONDO_CLARO "\033[48;5;250m"    // gris claro
+#define FONDO_PIEZA "\033[48;5;28m"     // blanco para piezas activas
 
 void inicializarTablero(Tablero* tablero) {
     for (int i = 0; i < 10; i++) {
@@ -13,7 +18,6 @@ void inicializarTablero(Tablero* tablero) {
     }
 }
 
-
 void limpiarTablero(Tablero* tablero) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -22,52 +26,38 @@ void limpiarTablero(Tablero* tablero) {
     }
 }
 
-#include <iostream>
-#include <string>
-using namespace std;
-
-#define RESET   "\033[0m"
-#define NEGRO   "\033[30m"
-#define BLANCO  "\033[97m"
-#define FONDO_OSCURO "\033[48;5;236m"   // gris oscuro
-#define FONDO_CLARO "\033[48;5;250m"    // gris claro
-#define FONDO_PIEZA "\033[48;5;28m"     // blanco para piezas activas
-
 void mostrarTablero(const Tablero* tablero) {
-    cout << "\n     ";
-    for (int j = 0; j < 10; j++) cout << j << "   ";
-    cout << "\n   +" << string(10 * 4, '-') << "+\n";
+    std::cout << "\n     ";
+    for (int j = 0; j < 10; j++) std::cout << j << "   ";
+    std::cout << "\n   +" << std::string(10 * 4, '-') << "+\n";
 
     for (int i = 0; i < 10; i++) {
-        cout << " " << i << " |";
+        std::cout << " " << i << " |";
         for (int j = 0; j < 10; j++) {
             Pieza* pieza = tablero->casillas[i][j];
             bool oscura = esCasillaOscura(i, j);
 
-         
-            string fondo = oscura ? FONDO_OSCURO : FONDO_CLARO;
+            std::string fondo = oscura ? FONDO_OSCURO : FONDO_CLARO;
 
             if (pieza != nullptr && estaActivaPieza(pieza)) {
                 char simbolo = obtenerSimbolo(pieza);
-                cout << fondo << " " << BLANCO << simbolo << " " << RESET << "|";
+                std::cout << fondo << " " << BLANCO << simbolo << " " << RESET << "|";
             } else {
-                cout << fondo << "   " << RESET << "|";
+                std::cout << fondo << "   " << RESET << "|";
             }
         }
-        cout << " " << i << "\n   +" << string(10 * 4, '-') << "+\n";
+        std::cout << " " << i << "\n   +" << std::string(10 * 4, '-') << "+\n";
     }
 
-    cout << "     ";
-    for (int j = 0; j < 10; j++) cout << j << "   ";
-    cout << "\n\n" << RESET;
-    cout << "Leyenda: b=peon blanco, B=DAMA blanca, n=peon negro, N=DAMA negra\n\n";
+    std::cout << "     ";
+    for (int j = 0; j < 10; j++) std::cout << j << "   ";
+    std::cout << "\n\n" << RESET;
+    std::cout << "Leyenda: b=peon blanco, B=DAMA blanca, n=peon negro, N=DAMA negra\n\n";
 }
-
 
 bool esCasillaOscura(int fila, int col) {
     return (fila + col) % 2 == 1;
 }
-
 
 bool esDentroDelTablero(int fila, int col) {
     return fila >= 0 && fila < 10 && col >= 0 && col < 10;
@@ -81,7 +71,6 @@ bool estaVaciaCasilla(const Tablero* tablero, int fila, int col) {
     return pieza == nullptr || !estaActivaPieza(pieza);
 }
 
-
 Pieza* obtenerPiezaEn(const Tablero* tablero, int fila, int col) {
     if (!esDentroDelTablero(fila, col)) {
         return nullptr;
@@ -93,7 +82,6 @@ Pieza* obtenerPiezaEn(const Tablero* tablero, int fila, int col) {
     return nullptr;
 }
 
-
 void colocarPiezaEn(Tablero* tablero, Pieza* pieza, int fila, int col) {
     if (esDentroDelTablero(fila, col)) {
         tablero->casillas[fila][col] = pieza;
@@ -104,7 +92,6 @@ void colocarPiezaEn(Tablero* tablero, Pieza* pieza, int fila, int col) {
     }
 }
 
-
 void eliminarPiezaEn(Tablero* tablero, int fila, int col) {
     if (esDentroDelTablero(fila, col)) {
         Pieza* pieza = tablero->casillas[fila][col];
@@ -114,7 +101,6 @@ void eliminarPiezaEn(Tablero* tablero, int fila, int col) {
         }
     }
 }
-
 
 void moverPiezaEn(Tablero* tablero, int origenF, int origenC, int destinoF, int destinoC) {
     if (esDentroDelTablero(origenF, origenC) && esDentroDelTablero(destinoF, destinoC)) {
@@ -128,13 +114,12 @@ void moverPiezaEn(Tablero* tablero, int origenF, int origenC, int destinoF, int 
     }
 }
 
-
 int contarPiezasColor(const Tablero* tablero, char color) {
     int count = 0;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             Pieza* pieza = tablero->casillas[i][j];
-            if (pieza != nullptr && estaActivaPieza(pieza) && 
+            if (pieza != nullptr && estaActivaPieza(pieza) &&
                 obtenerColorPieza(pieza) == color) {
                 count++;
             }
